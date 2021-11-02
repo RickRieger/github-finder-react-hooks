f0edd5ceea6b5f1df86ea24ca8076b24305aae6f
Rick Rieger <rickrieger36251@gmail.com>
2021-11-01 23:29:28 -0400

    clean up

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const NotFound = () => {
+  return (
+    <div>
+      <html>Not Found</html>
+      <p className='lead'>The page you are looking for does not exist.</p>
+    </div>
+  );
+};
+
+export default NotFound;
