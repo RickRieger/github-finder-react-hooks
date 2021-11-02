bf0fa7cd4cc47867d7a174d955357600e7080bc3
Rick Rieger <rickrieger36251@gmail.com>
2021-11-01 22:56:46 -0400

    finished

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,8 +1,8 @@
 import React, { useState, useContext } from 'react';
-import PropTypes from 'prop-types';
 import GithubContext from '../../context/github/githubContext';
-
-const Search = ({ setAlert }) => {
+import AlertContext from '../../context/alert/alertContext';
+const Search = () => {
+  const alertContext = useContext(AlertContext);
   const githubContext = useContext(GithubContext);
 
   const { clearUsers } = githubContext;
@@ -12,7 +12,7 @@ const Search = ({ setAlert }) => {
   const onSubmit = (e) => {
     e.preventDefault();
     if (text === '') {
-      setAlert('Please enter something', 'light');
+      alertContext.setAlert('Please enter something', 'light');
     } else {
       githubContext.searchUsers(text);
       setText('');
@@ -49,11 +49,6 @@ const Search = ({ setAlert }) => {
       </form>
     </div>
   );
-};
-
-Search.propTypes = {
-  // 'ptfr'
-  setAlert: PropTypes.func.isRequired,
 };
 
 export default Search;
